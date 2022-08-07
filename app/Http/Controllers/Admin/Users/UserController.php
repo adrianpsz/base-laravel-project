4caf61b838c6adce2f9a3940a7af541d85ed6011
adrian <adrian@adrianp.pl>
2022-08-07 16:48:09 +0200

    refactor Admin\UserController and use ApiUtils

--- app/Http/Controllers/Admin/Users/UserController.php
+++ app/Http/Controllers/Admin/Users/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Users;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use App\Traits\ApiUtils;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -14,6 +15,8 @@ use function view;
 
 class UserController extends Controller
 {
+    use ApiUtils;
+
     /**
      * @return Application|Factory|View|AnonymousResourceCollection
      * @throws AuthorizationException
@@ -24,12 +27,11 @@ class UserController extends Controller
 
         $users = User::paginate(self::ITEMS_PER_PAGE);
 
-        if (request()->wantsJson()) {
-            return UserResource::collection($users);
-        }
-
-        return view('admin.users.index', [
-            'users' => $users,
-        ]);
+        return $this->collectionWithView(
+            UserResource::collection($users),
+            view('admin.users.index', [
+                'users' => $users,
+            ])
+        );
     }
 }
